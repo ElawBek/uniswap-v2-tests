b4936a93230ee9ec9f88838653b868ab9eeea6b1
ElawBek <kdmitrii86@gmail.com>
2022-05-17 22:18:25 +0300

    init

---
+++ contracts/Token.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract Token is ERC20, Ownable {
+	constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+		_mint(msg.sender, 100 * 10**decimals());
+	}
+
+	function mint(address to, uint256 amount) public onlyOwner {
+		_mint(to, amount);
+	}
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,57 @@
+import * as dotenv from 'dotenv'
+
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomiclabs/hardhat-etherscan'
+import '@nomiclabs/hardhat-waffle'
+import '@typechain/hardhat'
+import 'hardhat-gas-reporter'
+import 'solidity-coverage'
+
+const config: HardhatUserConfig = {
+	solidity: {
+		compilers: [
+			{
+				version: '0.5.16',
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+				},
+			},
+			{
+				version: '0.6.6',
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+				},
+			},
+			{
+				version: '0.8.13',
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+				},
+			},
+		],
+	},
+	networks: {
+		ropsten: {
+			url: '',
+			accounts: [],
+		},
+	},
+	gasReporter: {
+		enabled: process.env.REPORT_GAS !== undefined,
+		currency: 'USD',
+	},
+	etherscan: {
+		apiKey: '',
+	},
+}
+
+export default config
